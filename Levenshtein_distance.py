df9c2352c015c7828e5c94f258607923341d21e3
Mikhail <firecrafter62@gmail.com>
2021-02-08 09:19:51 +0200

    Initial commit

---
+++ Levenshtein_distance.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def levenshtein_distance(a, b):
+    size_x = len(a.name) + 1
+    size_y = len(b) + 1
+    matrix = np.zeros((size_x, size_y))
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if a.name[x - 1] == b[y - 1]:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1],
+                    matrix[x, y - 1] + 1)
+            else:
+                matrix[x, y] = min(
+                    matrix[x - 1, y] + 1,
+                    matrix[x - 1, y - 1] + 1,
+                    matrix[x, y - 1] + 1)
+    return matrix[size_x - 1, size_y - 1]
